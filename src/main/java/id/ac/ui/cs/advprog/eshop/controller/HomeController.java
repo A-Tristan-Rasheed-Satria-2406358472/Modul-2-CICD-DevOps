d282f6f15b71908f8208e3db140f96477295f21a
Cyaside <tristan.rasheedsatria@gmail.com>
2026-02-09 09:06:36 +0700

    test : function homepage

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String root() {
-        return "redirect:/product/list";
+    public String homePage() {
+        return "HomePage";
     }
 
     @GetMapping("/product")
